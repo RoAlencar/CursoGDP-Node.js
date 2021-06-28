9f30f69ace971941d7fa6d65fb22ffa02fe9b610
RoAlencar <roalencarx@gmail.com>
2021-06-28 13:33:32 -0300

    Estruturas de repetição com EJS

--- Seção 5 - Projeto01 - Plataforma de Perguntas e Respostas com MySQL/index.js
+++ Seção 5 - Projeto01 - Plataforma de Perguntas e Respostas com MySQL/index.js
@@ -8,11 +8,24 @@ app.set('view engine','ejs');
 app.get("/:nome/:lang",(req,res)=> {
     var nome = req.params.nome;
     var lang = req.params.lang;
+    var exibirMsg = true;
+
+    var produtos = [
+        {nome: "Doritos", preco: 3.14},
+        {nome: "Coca-cola", preco: 5},
+        {nome: "Leite", preco: 1.45},
+        {nome: "Carne", preco: 23},
+        {nome: "Redbull", preco: 6},
+        {nome: "Nescau", preco: 4}
+    ]
+
     res.render("index",{
         nome : nome,
         lang: lang,
         empresa: "Rax Informatica",
-        inscritos: 8000
+        inscritos: 8000,
+        msg: exibirMsg,
+        produtos: produtos
     });
 });
 
