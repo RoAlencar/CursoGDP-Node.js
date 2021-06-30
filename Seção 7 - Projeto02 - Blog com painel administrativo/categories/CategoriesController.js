2155e2d129e70cb52f3fa1e6a36ca8c07d69226f
RoAlencar <roalencarx@gmail.com>
2021-06-30 16:44:07 -0300

    Deletanto categoria

--- Seção 7 - Projeto02 - Blog com painel administrativo/categories/CategoriesController.js
+++ Seção 7 - Projeto02 - Blog com painel administrativo/categories/CategoriesController.js
@@ -30,4 +30,25 @@ router.get('/admin/categories', (req,res) => {
     })
  
 })
+
+router.post("/categories/delete", (req,res) => {
+    var id = req.body.id;
+    if(id != undefined) {
+       if(!isNaN(id)){
+        Category.destroy({
+            where: {
+                id: id
+            }
+        }).then(() => {
+            res.redirect("/admin/categories");
+        })
+       }else{ // nao for um numero
+           res.redirect("/admin/categories");
+       }
+    } else { // NULL
+        
+        res.redirect("/admin/categories")
+    }
+})
+
 module.exports = router
