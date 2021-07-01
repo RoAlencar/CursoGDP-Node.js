7102f6524e3253b42c4c615d55d99c3811b8164f
RoAlencar <roalencarx@gmail.com>
2021-07-01 14:56:12 -0300

    Salvando artigos & Ajustando tempo no Sequelize

--- Seção 7 - Projeto02 - Blog com painel administrativo/database/database.js
+++ Seção 7 - Projeto02 - Blog com painel administrativo/database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guiapress','root','xxxi.x.mmxvii',{
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: "-03:00"
 });
 
 module.exports = connection;
